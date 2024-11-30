ed0592b13f26413d0d78a273cce0cdb158ceff57
vikas bhonde <bhondevikas831@gmail.com>
2024-11-30 18:11:06 +0530

    new change

--- FRONTEND/src/utils/constant.js
+++ FRONTEND/src/utils/constant.js
@@ -7,7 +7,7 @@
 
 // https://hiringplug.onrender.com/
 
-export const USER_API_END_POINT = `${process.env.URL}/api/v1/user`;
-export const JOB_API_END_POINT = `${process.env.URL}/api/v1/job`;
-export const APPLICATION_API_END_POINT = `${process.env.URL}/api/v1/applyapplication`;
-export const COMPANY_API_END_POINT = `${process.env.URL}/api/v1/company`;
+export const USER_API_END_POINT = `${process.env.REACT_APP_URL}/api/v1/user`;
+export const JOB_API_END_POINT = `${process.env.REACT_APP_URL}/api/v1/job`;
+export const APPLICATION_API_END_POINT = `${process.env.REACT_APP_URL}/api/v1/applyapplication`;
+export const COMPANY_API_END_POINT = `${process.env.REACT_APP_URL}/api/v1/company`;
