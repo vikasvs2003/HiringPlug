57f91146027cd2eab80b3cede3c38f01f729527a
VIKAS BHONDE <113436990+vikasvs2003@users.noreply.github.com>
2024-11-30 18:50:52 +0530

    Update constant.js
    url 2 changes

--- FRONTEND/src/utils/constant.js
+++ FRONTEND/src/utils/constant.js
@@ -1,13 +1,13 @@
 
-// export const USER_API_END_POINT = "http://localhost:8000/api/v1/user";
-// export const JOB_API_END_POINT = "http://localhost:8000/api/v1/job";
-// export const APPLICATION_API_END_POINT = "http://localhost:8000/api/v1/applyapplication";
-// export const COMPANY_API_END_POINT="http://localhost:8000/api/v1/company";
+export const USER_API_END_POINT = "https://hiringplug.onrender.com/api/v1/user";
+export const JOB_API_END_POINT = "https://hiringplug.onrender.com/api/v1/job";
+export const APPLICATION_API_END_POINT = "https://hiringplug.onrender.com/api/v1/applyapplication";
+export const COMPANY_API_END_POINT="https://hiringplug.onrender.com/api/v1/company";
 
 
-// https://hiringplug.onrender.com/
+// https://hiringplug.onrender.com
 
-export const USER_API_END_POINT = `${process.env.REACT_APP_URL}/api/v1/user`;
-export const JOB_API_END_POINT = `${process.env.REACT_APP_URL}/api/v1/job`;
-export const APPLICATION_API_END_POINT = `${process.env.REACT_APP_URL}/api/v1/applyapplication`;
-export const COMPANY_API_END_POINT = `${process.env.REACT_APP_URL}/api/v1/company`;
+// export const USER_API_END_POINT = `${process.env.REACT_APP_URL}/api/v1/user`;
+// export const JOB_API_END_POINT = `${process.env.REACT_APP_URL}/api/v1/job`;
+// export const APPLICATION_API_END_POINT = `${process.env.REACT_APP_URL}/api/v1/applyapplication`;
+// export const COMPANY_API_END_POINT = `${process.env.REACT_APP_URL}/api/v1/company`;
