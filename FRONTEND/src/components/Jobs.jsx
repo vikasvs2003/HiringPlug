2df00825d54dbbccdd454a95528ca8658c7440d9
VIKAS BHONDE <113436990+vikasvs2003@users.noreply.github.com>
2024-11-30 18:17:03 +0530

    Update Jobs.jsx
    option chaining change

--- FRONTEND/src/components/Jobs.jsx
+++ FRONTEND/src/components/Jobs.jsx
@@ -46,13 +46,13 @@ function Jobs() {
                     </div>
 
                     {
-                        filterJobs.length <= 0 ? <span> Job Not Found </span> : (
+                        filterJobs?.length <= 0 ? <span> Job Not Found </span> : (
                             <div className='flex-1  h-[88vh] overflow-y-auto pb-5' >
                                 <div className='grid grid-cols-3 gap-4'>
 
                                     {
 
-                                        filterJobs.map((job) => (
+                                        filterJobs?.map((job) => (
                                             <motion.div
                                                 initial={{ opacity: 0, x: 100 }}
                                                 animate={{ opacity: 1, x: 0 }}
